731078f634ed3f7f048dcc614318f603536853b3
javeriishika <126329582+javeriishika@users.noreply.github.com>
2024-05-08 23:29:43 +0530

    installed ND used mongodb in the backend of the signup page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 
 
-export const app = express();
+import Connection from './databse/db.js';
 
 
+const app = express();
 
+
+const PORT = 8000;
+
+app.listen(PORT, ()=> console.log(`server is running successfully on PORT ${PORT}`));
+
+Connection();
